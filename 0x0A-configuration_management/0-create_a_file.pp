056408491ec740f1e43a8937705ae371f0932c31
ISAIAH-max <70812052+ISAIAH-max@users.noreply.github.com>
2021-04-06 23:31:46 +0300

    Delete 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create a file with content using Puppet
-
-file { '/tmp/holberton':
-    ensure  => 'directory',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-}
