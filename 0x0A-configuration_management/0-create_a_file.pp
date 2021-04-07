b4ba9f94760ca689ad55fcd9f3783d11e1108f54
ISAIAH-max <mlewaisaiah@gmail.com>
2021-04-07 12:25:21 +0300

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file using Puppet
+
+file {'/tmp/holberton':
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
